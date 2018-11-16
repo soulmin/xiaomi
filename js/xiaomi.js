35d44070c91688882a45d7e46a2482ddf91e11b3
ChenMin <ChenMin>
2018-11-16 14:28:23 +0800

    图片不移动

--- js/xiaomi.js
+++ js/xiaomi.js
@@ -6,8 +6,69 @@ $(document).ready(function(){
     $(".phone").css("display","none");
   });
   
+  var sidebox = $(".sidebox")
   var ul = $(".lunbo");
   var dot = $(".ellipsis");
   var onewidth = ul.find("li").eq(0).width();
   var num = dot.find("i");
-});
+  var sw = 0;
+  var timer = null;
+  //每个i绑定click事件，完成切换颜色和动画，以及读取参数值
+  num.on("click",function(){
+  	$(this).addClass("active").siblings("i").removeClass("active");
+  	sw = $(this).index();
+  	ul.animate({
+  			"right":onewidth*sw,		
+  	});
+  	ul.fadeIn(onewidth*sw).siblings().fade()
+  });
+  
+  //左右按钮的控制效果
+  $(".next").stop(true,true).click(function(){
+  		sw++;
+  		if(sw == num.length){
+  			sw = 0;
+  		}
+  		num.eq(sw).trigger("click");
+  		
+  });
+  
+ 	$(".prev").stop(true,true).click(function(){
+ 			sw--;
+ 			if(sw == num.length){
+ 				sw = 0;
+ 			}
+ 			num.eq(sw).trigger("click");
+ 	});
+ 
+ 
+ //定时器的使用，自动开始
+ 		timer = setInterval(function(){
+ 				sw++;
+ 				if(sw == num.length){
+ 					sw = 0;
+ 				}
+ 				num.eq(sw).trigger("click");
+ 		},3000);
+ 		
+ 	//hover事件完成悬停和，左右图标的动画效果
+ 		sidebox.hover(function(){
+ 				$(".next,.prev").animate({
+ 						"opacity":1,
+ 				},200);
+ 				
+ 				clearInterval(timer);
+ 		},function(){
+ 				$(".next,.prev").animate({
+ 						"opacity":0.5,
+ 				},500);
+ 				
+ 				timer = setInterval(function(){
+ 					sw++;
+ 					if(sw == num.length){
+ 							sw = 0;
+ 					}
+ 					num.eq(sw).trigger("click");
+ 				},2000);
+ 		});
+});		
